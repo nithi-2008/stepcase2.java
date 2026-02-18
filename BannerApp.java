5b77af8d282bda925455bc94528532ddc9c36f99
Nithi-2008 <av2905@srmist.edu.in>
2026-02-18 10:02:12 +0530

    Added UC1 implementation

---
+++ BannerApp.java
@@ -0,0 +1,21 @@
+class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            "  ****     ****    ******    ****** ",
+            " **  **   **  **   **   **   **     ",
+            "**    **  **    **  **   **   **     ",
+            "**    **  **    **  ******    *****  ",
+            "**    **  **    **  **        **     ",
+            " **  **   **  **   **        **     ",
+            "  ****     ****    **        ****** "
+        };
+
+        // Print using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
